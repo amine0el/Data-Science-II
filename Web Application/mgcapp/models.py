f078e4e4c52a4f31e025439a8b06b7688fb8fbdd
el blidi <amine.el.blidi@gmx.de>
2022-11-25 20:06:09 +0100

    changes

--- Web Application/mgcapp/models.py
+++ Web Application/mgcapp/models.py
@@ -6,3 +6,5 @@ class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+
