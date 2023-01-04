6f23b54254bcf75073780c9ada268a16a4e8fefe
el blidi <amine.el.blidi@gmx.de>
2023-01-04 16:49:01 +0100

    new models

--- Web Application/mgcapp/forms.py
+++ Web Application/mgcapp/forms.py
@@ -6,4 +6,4 @@ from mgcapp.models import Document
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
-        fields = ('description', 'document', )
+        fields = ('name', 'document', 'prediction')
