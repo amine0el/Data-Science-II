0d378944a79027f4d8069db475b7c2954bd470b7
Dustin Reymendt <81078529+DustinReymendt@users.noreply.github.com>
2023-01-13 11:37:34 +0100

    Changes to recommender script

--- Web Application/mgcapp/recommender.py
+++ Web Application/mgcapp/recommender.py
@@ -5,11 +5,14 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn import preprocessing
 import os
-general_path = '../Data'
 
-def get_extraction_data():
+def read_features():
+    df = pd.read_csv('../features_last_song.csv')
+    return df
+
+def get_extraction_similarity(dataframe, name, part):
     # Read data
-    data = pd.read_csv(f'{general_path}/extraction.csv', index_col='name_v')
+    data = dataframe
 
     # Extract labels
     labels = data[['genre']]
@@ -20,12 +23,8 @@ def get_extraction_data():
 
     # Scale the data
     data_scaled=preprocessing.scale(data)
-    return data_scaled
-
-
-def cosine_similarity():
-    # Cosine similarity
-    similarity = cosine_similarity(data_scaled)
+   
+    similarity = cosine_similarity(data)
     #print("Similarity shape:", similarity.shape)
 
     # Convert into a dataframe and then set the row index and column names as labels
@@ -33,10 +32,7 @@ def cosine_similarity():
     sim_df_names = sim_df_labels.set_index(labels.index)
     sim_df_names.columns = labels.index
     sim_df_names.head()
-    return similarity
-
-def find_similar_songs(name,part):
-    # Find songs most similar to another song
+     # Find songs most similar to another song
     series = sim_df_names[name+part].sort_values(ascending = False)
     
     
@@ -50,12 +46,12 @@ def find_similar_songs(name,part):
     #print(series.head(5))
     return series.head(5)
 
-def play_similar_song():
-    #index = 1;
-    #if(index > 5): 
-    #return "Index out of bounds, please stay below 5!"
-    return "Playing: " + similar_song.index[index -1]
-    #song_genre = similar_song.index[index -1]
-    #song_genre = song_genre.split('.')
-    #songname = similar_song.index[0].split('.')
-    #ipd.Audio(f'{general_path}/genres_original/' + song_genre[0] + '/' + song_genre[0] + "." + songname[1] + '.wav')
+# def play_similar_song():
+#     #index = 1;
+#     #if(index > 5): 
+#     #return "Index out of bounds, please stay below 5!"
+#     return "Playing: " + similar_song.index[index -1]
+#     #song_genre = similar_song.index[index -1]
+#     #song_genre = song_genre.split('.')
+#     #songname = similar_song.index[0].split('.')
+#     #ipd.Audio(f'{general_path}/genres_original/' + song_genre[0] + '/' + song_genre[0] + "." + songname[1] + '.wav')
--- Web Application/mgcapp/views.py
+++ Web Application/mgcapp/views.py
@@ -65,7 +65,22 @@ def extraction_view(request):
 
     
 def recommender_view(request):
-    # df = load_csv("../feature_last_song.csv")
+    df_input = pd.read_csv('features_last_song.csv')
+    df_extraction = pd.read_csv('extraction.csv')
+    df_extraction_scaled = df_extraction.drop(columns=['genre'])
+    df_input_scaled = df_input.drop(columns=['start'])
+    df_combined = pd.concat([df_input_scaled, df_extraction_scaled])
+    song = df_input.head(1)
+    song_name_part = song['name_v'][0]
+    temp = song_name_part.split(".")
+    song_name = temp[0]+"."+temp[1]
+    song_part = "."+temp[2]
+    #documents = Document.objects.last()
+    #documents.recommender_text = get_extraction_similarity(df_combined, song_name, song_part)
+    
+    #df_input =
+    #cosine_similarity = get_extraction_similarity(df_combined, name, part)
+    #last_song = df['name_v'][0]
     # string = "Last Song is : " + df['name_v'][0]
     # df_db = load_csv("extraction.csv")
     # new_df= combine df and df_db
@@ -82,6 +97,7 @@ def recommender_view(request):
     
     #documents.recommender = recom
     #documents.prediction_text = pred_text
+    #documents.recommender_text = recom_text
     #documents.save()
     return render(request, 'mgcapp/recommender.html')
 
