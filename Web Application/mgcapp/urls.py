b968de7dfe590c39b0fa01b6bd952a565a95a41d
CarstenKleber <110473427+CarstenKleber@users.noreply.github.com>
2023-01-18 20:42:43 +0100

    moved songs to media and prediction

--- Web Application/mgcapp/urls.py
+++ Web Application/mgcapp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("upload", views.simple_upload, name="upload"),
     path("prediction", views.extraction_view, name="prediction"),
     path("recommender", views.recommender_view, name="recommender"),
-   
+    path("discommender", views.recommender_view_worst, name="recommender-worst"),
 ]
