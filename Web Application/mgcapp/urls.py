78665146e1a07a54fc8b0b7e65f16e8579809104
CarstenKleber <110473427+CarstenKleber@users.noreply.github.com>
2023-01-28 11:33:37 +0100

    Further comments

--- Web Application/mgcapp/urls.py
+++ Web Application/mgcapp/urls.py
@@ -1,3 +1,14 @@
+
+# GenRec - The Smash Group
+    # Music Genre Recommender and Classifier
+    # Project during Data Science 2
+    # WiSe 2022/2023
+    # TU Darmstadt
+
+# File urls.py
+    # Description: 
+        # Django-File were the urls are coded into functions in the file views.py, needed to define the pages/functions of the website
+
 from django.urls import path
 from mgcapp import views
 
